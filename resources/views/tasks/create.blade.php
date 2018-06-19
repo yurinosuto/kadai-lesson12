386b948878aee85b8e7e219c1ed62f24ae0c15eb
yurino.suto <ec2-user@ip-172-31-36-30.us-east-2.compute.internal>
2018-06-19 05:27:06 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,17 +4,23 @@
 
     <h1>作成ページ</h1>
 
-
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
-
+    <div class="row">
+        <div class="col-xs-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
 @endsection
