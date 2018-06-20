a49e46161c45f5cb65f0606417c2f94efb71fdc7
yurino.suto <ec2-user@ip-172-31-36-30.us-east-2.compute.internal>
2018-06-20 07:08:02 +0000

    procfile

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,7 +4,7 @@
 
     <h1>タスク一覧</h1>
 
-   @if (count($tasks) > 0)
+    @if (count($tasks) > 0)
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -27,4 +27,7 @@
     
     {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
 
+    
+    
+    
 @endsection
